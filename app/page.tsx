199b90edb0f4f3ff72e30392b3984ed097cde0ed
beecodeguy <beecodeguy@gmail.com>
2024-05-29 18:43:21 +0545

    sentry installed and portfolio completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
+import Clients from "@/components/clients";
+import Experiences from "@/components/experiences";
+import Footer from "@/components/footer";
 import Grid from "@/components/grid";
 import Hero from "@/components/hero";
+import RecentProjects from "@/components/recent-projects";
 import { FloatingNav } from "@/components/ui/floating-navbar";
+import WorkApproach from "@/components/work-approach";
 import { navItems } from "@/data";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experiences />
+        <WorkApproach />
+        <Footer />
       </div>
-    </main>
+    </main> 
   );
 }
